0ec29c98fb9bd3e1c491f8834cb63765d214484c
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2012-05-17 10:51:48 +0900

    Initial Commit

---
+++ ngram-converter-cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ngram-converter-cpp
+//
+//  Created by 宏史 真鍋 on 12/05/17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
