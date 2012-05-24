e5e1963bf3b323a95d280796818bb09ba91611ee
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2012-05-24 16:33:06 +0900

    Removed main.cpp.

--- ngram-converter-cpp/main.cpp
+++ ngram-converter-cpp/main.cpp
@@ -2,17 +2,13 @@
 //  main.cpp
 //  ngram-converter-cpp
 //
-//  Created by 宏史 真鍋 on 12/05/17.
+//  Created by Hiroshi Manabe on 12/05/17.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
-
-#include <iostream>
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
     return 0;
 }
 
