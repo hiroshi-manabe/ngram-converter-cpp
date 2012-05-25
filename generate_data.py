e25cb5d2439569c9605d9db9e11590c85d939b64
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2012-05-25 19:38:35 +0900

    Fixed bugs.

--- generate_data.py
+++ generate_data.py
@@ -19,13 +19,15 @@ EXT_DATA = '.data'
 def Usage():
     print sys.argv[0] + ' - generate N-gram data from a sorted N-gram file.'
     print 'Usage: '  + sys.argv[0] + \
-        '-i <input filename> ' + \
+        '(-i <input filename>|-v <verification filename) ' + \
         '-o <output filename prefix> ' + \
-        '-s <min score> ' + \
-        '[-v <verification filename>]' + \
-        '[-d (only execute verification)]'
+        '-s <min score> '
     sys.exit()
 
+
+def gen_test():
+    for i in range(128):
+        yield 1
 
 class BitArray(object):
     def __init__(self):
@@ -51,7 +53,9 @@ class BitArray(object):
 
     def SetByteArray(self, b_array, length):
         if int(length / 8) != len(b_array):
-        self._bytearray = bytearray(array)
+            raise ValueError
+
+        self._bytearray = bytearray(b_array)
         self._length = length
 
 
@@ -84,13 +88,14 @@ def DecodeNumber(b):
     elif b[0] & (1 << 6):
         x = ((b[0] & ((1 << 6) - 1)) << 8) | b[1]
         length = 2
-    elif p[0] & (1 << 5):
+    elif b[0] & (1 << 5):
         x = ((b[0] & ((1 << 5) - 1)) << 16) | (b[1] << 8) | b[2]
         length = 3
-    elif p[0] & (1 << 4):
+    elif b[0] & (1 << 4):
         x = ((b[0] & ((1 << 4) - 1)) << 24) | (b[1] << 16) | (b[2] << 8) | b[3]
+        length = 4
 
-    return (x, len)
+    return (x, length)
 
 
 class NgramBlock(object):
@@ -139,6 +144,10 @@ class NgramBlock(object):
         self._context_data += EncodeNumber(
             context_id - (self._prev_context_id if
                           self._prev_token_id == token_id else 0))
+
+        self._prev_token_id = token_id
+        self._prev_context_id = context_id
+
         return self._size >= self._max_size
 
     def GetFirstIds(self):
@@ -162,24 +171,29 @@ class NgramBlock(object):
             data += self._backoff_bits.GetByteArray()
             data += self._backoff_data
 
-        data += self._token_data.GetByteArray()
+        if self._store_token_data:
+            data += self._token_data.GetByteArray()
+
         data += self._context_data
 
         self.__init__(self._max_size)
         return data
 
+def EncodeScore(x, min_score):
+    return int(x * MAX_INT_SCORE / min_score)
 
-def DecodeFiles(output_filename_prefix, verification_filename, block_size, min_score):
-    def decode_score(x):
-        return x * min_score / MAX_INT_SCORE
+def DecodeScore(x, min_score):
+    return x * min_score / MAX_INT_SCORE
 
+def DecodeFiles(output_filename_prefix, verification_filename, block_size,
+                min_score):
     def decode_block(buf, block_size, token_ids, context_ids, scores, backoffs):
         offset = 0
         flags = buf[offset]
         offset += 1
 
         for i in range(block_size):
-            scores[i] = decode_score(buf[offset])
+            scores[i] = DecodeScore(buf[offset], min_score)
             offset += 1
 
         if flags & FLAGS_HAS_BACKOFF:
@@ -191,40 +205,54 @@ def DecodeFiles(output_filename_prefix, verification_filename, block_size, min_s
             i = 0
             for b in backoff_bits.IterBits():
                 if b:
-                    backoffs[i] = buf[offset]
+                    backoffs[i] = DecodeScore(buf[offset] -
+                                              BACKOFF_DISCOUNT,
+                                              min_score)
                     offset += 1
                 else:
                     backoffs[i] = None
 
-        if flags & FLAGS_TOKEN_IDS:
+                i += 1
+
+        if flags & FLAGS_HAS_TOKEN_IDS:
             token_block_len = 0
             token_bits = BitArray()
             token_bits.SetByteArray(buf[offset:], len(buf[offset:]) * 8)
 
-            for i in range(1, block_size):
-                count = 0
-                bit_len = 0
+            i = 1
+            bit_len = 0
+            count = 0
 
-                for b in token_bits.IterBits():
-                    bit_len += 1
-                    if b:
+            for b in token_bits.IterBits():
+                bit_len += 1
+                if b:
+                    token_ids[i] = token_ids[i-1] + count
+                    token_block_len += count + 1
+                    count = 0
+                    i += 1
+                    if i >= block_size:
                         break
+
+                else:
                     count += 1
 
-                token_ids[i] = token_ids[i-1] + count
-                token_block_len += count + 1
+            offset += int((token_block_len + 7) / 8)
 
-            offset += int(token_block_len / 8)
-
-        else: # flags & FLAGS_TOKEN_IDS
+        else: # flags & FLAGS_HAS_TOKEN_IDS
             token_ids[1:block_size] = [token_ids[0]] * (block_size - 1)
 
         for i in range(1, block_size):
-            (context_id_offset, length) = DecodeNumber(token_ids[offset:])
-            context_ids[i] = context_ids[i-1] + context_id_offset
+            (context_id_offset, length) = DecodeNumber(buf[offset:])
+            context_ids[i] = context_id_offset
+
+            if token_ids[i] == token_ids[i-1]:
+                context_ids[i] += context_ids[i-1]
+
             offset += length
 
         if offset != len(buf):
+            print 'token_ids: %s context_ids: %s offset: %d len(buf): %d' % (
+                (token_ids,), (context_ids,), offset, len(buf))
             raise ValueError
 
         return
@@ -239,10 +267,14 @@ def DecodeFiles(output_filename_prefix, verification_filename, block_size, min_s
     while True:
         cur_n += 1
         buf = f_in_index.read(4)
-        cur_ngram_count = struct.unpack('=L', buf)
-        f_out.write('#%d\t%d' % (cur_n, cur_ngram_count))
+        if not buf:
+            break
 
-        prev_data_offset = 0
+        cur_ngram_count = struct.unpack('=L', buf)[0]
+        f_out.write('#%d\t%d\n' % (cur_n, cur_ngram_count))
+        print 'Processing %d-gram...' % cur_n
+
+        prev_data_offset = f_in_data.tell()
         data_offset = None
 
         token_ids = [0] * block_size
@@ -254,29 +286,45 @@ def DecodeFiles(output_filename_prefix, verification_filename, block_size, min_s
 
         for i in range(cur_ngram_count):
             if cur_n == 1:
-                buf = f_in_index.read(2)
-                (score, backoff) = (decode_score(x) for x in buf)
-                f_out.write('%d\t%d\t%d\t%f\t%f' % (i, i, 0, score, backoff))
+                buf = bytearray(f_in_index.read(2))
+                score = DecodeScore(buf[0], min_score)
+                backoff = DecodeScore(buf[1] - BACKOFF_DISCOUNT, min_score)
+                f_out.write('%d\t%d\t%d\t%f\t%f\n' % (i, i, 0, score, backoff))
 
             else: # cur_n != 1
-                if i % block_size == 0:
+                block_index = i % block_size
+                if block_index == 0:
                     buf = f_in_index.read(12)
                     (token_ids[0], context_ids[0], data_offset) = struct.unpack('=LLL', buf)
                     if prev_data_offset != f_in_data.tell():
                         raise ValueError
 
-                    buf = f_in_data.read(data_offset - prev_data_offset)
-                    decode_block(buf, block_size, token_ids, context_ids, scores, backoffs)
+                    data_buf = bytearray(f_in_data.read(
+                            data_offset - prev_data_offset))
+                    decode_block(data_buf, block_size, token_ids, context_ids,
+                                 scores, backoffs)
                     prev_data_offset = data_offset
 
-                f_out.write('%d\t%d\t%d\t%f\t%f' % (i,
-                                                    token_ids[i % block_size],
-                                                    context_ids[i % block_size],
-                                                    scores[i % block_size],
-                                                    -99.0 if backoffs[i] is None
-                                                    else backoffs[i])
+                f_out.write('%d\t%d\t%d\t%f\t%f\n' % (
+                        i,
+                        token_ids[block_index],
+                        context_ids[block_index],
+                        scores[block_index],
+                        -99.0 if backoffs[block_index] is None
+                        else backoffs[block_index]))
+
+    print 'Done.'
 
 def main():
+    def output_data(f_out_index, f_out_data, ngram_block):
+        (first_token_id, first_context_id) = ngram_block.GetFirstIds()
+
+        data = ngram_block.FlushData()
+        f_out_data.write(data)
+
+        f_out_index.write(struct.pack('=LLL', first_token_id,
+                                      first_context_id, f_out_data.tell()))
+        
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'hi:o:s:v:')
     except getopt.GetoptError:
@@ -304,14 +352,13 @@ def main():
         elif k == '-v':
             verification_filename = v
 
-    if not output_filename_prefix or not min_score:
+    if not output_filename_prefix or not min_score or (
+        not input_filename and not verification_filename):
         Usage()
 
-    if flag_verification_only:
-        if not verification_filename:
-            Usage()
-
-        DecodeFiles(output_filename_prefix, verification_filename)
+    if not input_filename and verification_filename:
+        DecodeFiles(output_filename_prefix, verification_filename, BLOCK_SIZE,
+                    min_score)
         sys.exit(0)
 
     if not input_filename:
@@ -323,7 +370,6 @@ def main():
 
     cur_n = 0
     cur_ngram_count = 0
-    data_offset = 0
     ngram_block = NgramBlock(BLOCK_SIZE)
     was_full = False
 
@@ -335,14 +381,8 @@ def main():
         fields = line.split('\t')
 
         if fields[0][0] == '#':
-            if cur_n == 1:
-                if cur_ngram_count % 2 == 1:
-                    f_out_index.write(struct.pack('=BB', 0, 0))
-
-            else: # cur_n != 1
-                data = ngram_block.FlushData()
-                f_out_data.write(data)
-                data_offset += len(data)
+            if cur_n > 1:
+                output_data(f_out_index, f_out_data, ngram_block)
 
             new_n = int(fields[0][1:])
             if new_n != cur_n + 1:
@@ -350,6 +390,7 @@ def main():
 
             cur_n = new_n
             cur_ngram_count = int(fields[1])
+            print 'Processing %d-gram...' % cur_n
 
             f_out_index.write(struct.pack('=L', cur_ngram_count))
             
@@ -361,7 +402,7 @@ def main():
         if cur_n == 1 and backoff == -99.0:
             backoff = 0.0
 
-        (score_int, backoff_int) = (int(x / min_score * MAX_SCORE) for x
+        (score_int, backoff_int) = (EncodeScore(x, min_score) for x
                                     in (score, backoff))
 
         backoff_int += BACKOFF_DISCOUNT
@@ -385,14 +426,9 @@ def main():
             is_full = ngram_block.AddNgram(token_id, context_id,
                                            score_int, backoff_int)
             if is_full:
-                data_offset += len(data)
-                (first_token_id, first_context_id) = ngram_block.GetFirstIds()
-                f_out_index.write(struct.pack('=LLL', first_token_id,
-                                              first_context_id, data_offset))
+                output_data(f_out_index, f_out_data, ngram_block)
 
-                data = ngram_block.FlushData()
-                f_out_data.write(data)
-
+    output_data(f_out_index, f_out_data, ngram_block)
     f_in.close()
     f_out_index.close()
     f_out_data.close()
