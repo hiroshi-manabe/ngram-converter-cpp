5197f56471a344993afe5eb577c8c895f2aa113c
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2012-05-30 12:15:13 +0900

    Added "key" trie.

--- sort-ngram.py
+++ sort-ngram.py
@@ -11,12 +11,16 @@ import sys
 #externals
 import marisa
 
+EXT_KEY = '.key'
+EXT_PAIR = '.pair'
+EXT_TEXT = '.lmtext'
+PAIR_SEPARATOR = '/'
+
 def Usage():
     print sys.argv[0] + ' - sort SRILM format ngram file.'
     print 'Usage: '  + sys.argv[0] + \
         '-i <input filename> ' + \
-        '-o <output text filename> ' + \
-        '-t <output trie filename> '
+        '-o <output filename prefix> '
     sys.exit()
 
 def to_id(trie, agent, query_key):
@@ -49,8 +53,7 @@ def main():
         sys.exit(2)
 
     input_filename = ''
-    output_text_filename = ''
-    output_trie_filename = ''
+    output_filename_prefix = ''
 
     for k, v in opts:
         if k == '-h':
@@ -59,24 +62,23 @@ def main():
         elif k == '-i':
             input_filename = v
         elif k == '-o':
-            output_text_filename = v
-        elif k == '-t':
-            output_trie_filename = v
+            output_filename_prefix = v
 
-    if (input_filename == '' or output_text_filename == ''
-        or output_trie_filename == ''):
+    if input_filename == '' or output_filename_prefix == '':
         Usage()
 
     f_in = codecs.open(input_filename, 'r', 'utf-8')
-    f_out = codecs.open(output_text_filename, 'w', 'utf-8')
+    f_out_text = codecs.open(output_filename_prefix + EXT_TEXT, 'w', 'utf-8')
 
     cur_n = 0
-    word_id = 1
-    word_dict = {}
+    pair_id = 1
+    pair_dict = {}
     context_dict = {}
     context_id_dicts = [{}]
-    keyset_word = marisa.Keyset()
-    trie_word = marisa.Trie()
+    keyset_key = marisa.Keyset()
+    keyset_pair = marisa.Keyset()
+    trie_key = marisa.Trie()
+    trie_pair = marisa.Trie()
     agent = marisa.Agent()
     min_score = 99.0
     max_backoff = -99.0
@@ -90,12 +92,14 @@ def main():
             m = re.search(r'^\\(\d+)-grams:', line)
             if (cur_n and (m or re.search(r'^\\end\\', line))):
                 if  cur_n == 1:
-                    trie_word.build(keyset_word)
-                    trie_word.save(output_trie_filename)
-                    for k, v in word_dict.iteritems():
-                        context_dict[(to_id(trie_word, agent, k),)] = v
+                    trie_key.build(keyset_key)
+                    trie_key.save(output_filename_prefix + EXT_KEY)
+                    trie_pair.build(keyset_pair)
+                    trie_pair.save(output_filename_prefix + EXT_PAIR)
+                    for k, v in pair_dict.iteritems():
+                        context_dict[(to_id(trie_pair, agent, k),)] = v
 
-                output_ngram(f_out, cur_n, context_dict, context_id_dicts)
+                output_ngram(f_out_text, cur_n, context_dict, context_id_dicts)
 
             if m:
                 cur_n = int(m.group(1))
@@ -124,15 +128,17 @@ def main():
 
         if cur_n == 1:
             k = fields[1].encode('utf-8')
-            keyset_word.push_back(k)
-            word_dict[k] = (float(fields[0]), float(fields[2]), fields[1])
+            keyset_pair.push_back(k)
+            pair = k.split(PAIR_SEPARATOR, 1);
+            keyset_key.push_back(pair[0])
+            pair_dict[k] = (float(fields[0]), float(fields[2]), fields[1])
         else:
-            ngram = [to_id(trie_word, agent, x.encode('utf-8')) for x
+            ngram = [to_id(trie_pair, agent, x.encode('utf-8')) for x
                      in reversed(fields[1].split(' '))]
             context_dict[tuple(ngram)] = (float(fields[0]), float(fields[2]), fields[1])
 
     f_in.close()
-    f_out.close()
+    f_out_text.close()
     print 'Done.'
     print 'min_score = %f, max_backoff = %f' % (min_score, max_backoff)
 
